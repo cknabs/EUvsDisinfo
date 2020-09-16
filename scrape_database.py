950d83fd71c7493878f6640abfefe3b5f09b98b9
Christian Knabenhans <christian@knabenhans.fr>
2020-09-16 18:29:31 +0200

    Scrape additional data from report pages, switch to CSV

--- scrape_database.py
+++ scrape_database.py
@@ -1,5 +1,5 @@
 # Scrape the euvsdisinfo.eu database and produce a .json of the entries
-import json
+from csv import DictWriter
 from datetime import datetime
 
 import requests as req
@@ -20,20 +20,64 @@ def get_data_column(soup, data_col):
     assert (len(data_columns) == 1)
     return data_columns[0]
 
-# TODO: use API to scrape N entries, use date ranges, etc.
-for post in soup.find_all('div', attrs={'class': 'disinfo-db-post'}):
-    post_dict = {}
-    d = get_data_column(post, 'Date').contents[0].strip()
-    post_dict['date'] = datetime.strptime(d, '%d.%m.%Y').date().isoformat()
-    title_link = get_data_column(post, 'Title').find('a')
-    title = title_link.contents[0].strip()
-    id = title_link['href']
-    post_dict['title'] = title
-    post_dict['id'] = id
-    outlets = [o.strip() for o in get_data_column(post, 'Outlets').contents[0].split(',')]
-    post_dict['outlets'] = outlets
-    countries = [c.strip() for c in get_data_column(post, 'Country').contents[0].split(',')]
-    post_dict['countries'] = countries
 
-    # TODO: use `id' to get more info (language, article urls, summary, disproof)
-    print(json.dumps(post_dict))
+# TODO: use API to scrape N entries, use date ranges, etc.
+POST_KEYS = ['date', 'id', 'title', 'countries', 'keywords']
+ANNOTATION_KEYS = ['id', 'summary', 'disproof']
+PUBLICATION_KEYS = ['id', 'language', 'outlet', 'publication', 'archive']
+with open('posts.csv', 'w') as posts_file, \
+        open('annotations.csv', 'w') as annotations_file, \
+        open('publications.csv', 'w') as publications_file:
+    posts_writer = DictWriter(posts_file, POST_KEYS)
+    annotations_writer = DictWriter(annotations_file, ANNOTATION_KEYS)
+    publications_writer = DictWriter(publications_file, PUBLICATION_KEYS)
+
+    for post in soup.find_all('div', attrs={'class': 'disinfo-db-post'}):
+        # Get basic data from database entry
+        date = get_data_column(post, 'Date').contents[0].strip()
+        iso_date = datetime.strptime(date, '%d.%m.%Y').date().isoformat()
+        title_link = get_data_column(post, 'Title').find('a')
+        title = title_link.contents[0].strip()
+        id = title_link['href']
+
+        outlets = [o.strip() for o in get_data_column(post, 'Outlets').contents[0].split(',')]
+        countries = [c.strip() for c in get_data_column(post, 'Country').contents[0].split(',')]
+
+        # Get keywords, summary, disproof from report page
+        report = BeautifulSoup(req.get(id).text, 'html.parser')
+
+        keywords = report.find(text='Keywords:').parent.parent.contents[-1].strip()
+        summary = report.find(attrs={'class': 'b-report__summary-text'}).text.strip()
+        disproof = report.find(attrs={'class': 'b-report__disproof-text'}).text.strip()
+
+        # Get data on articles from report page
+        articles = report.find_all(attrs={'class': 'b-catalog__link'})
+        languages = [l.strip() for l in report.find(text='Language/target audience:').next.split(',')]
+        assert (len(articles) == len(languages))
+        assert (len(articles) == len(outlets))
+
+        # Output data
+
+        posts_writer.writerow({
+            'date': iso_date,
+            'id': id,
+            'title': title,
+            'countries': countries,
+            'keywords': keywords
+        })
+
+        annotations_writer.writerow({
+            'id': id,
+            'summary': summary,
+            'disproof': disproof
+        })
+
+        for article, outlet, language in zip(articles, outlets, languages):
+            link, archived_link = [a['href'] for a in article.find_all('a')]
+            publications_writer.writerow({
+                'id': id,
+                'language': language,
+                'outlet': outlet,
+                'publication': link,
+                'archive': archived_link
+            })
