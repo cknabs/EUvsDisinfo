dcabbd6ff8bb9d5d0a5613567476da71419b611f
Christian Knabenhans <christian@knabenhans.fr>
2020-09-17 19:50:11 +0200

    Handle missing data more gracefully

--- scrape_database.py
+++ scrape_database.py
@@ -2,6 +2,7 @@
 from csv import DictWriter
 from datetime import datetime
 from typing import List, Tuple
+from warnings import warn
 
 import requests as req
 from bs4 import BeautifulSoup
@@ -29,20 +30,20 @@ def all_entries():
 
 
 class Entry:
-    iso_date: str
-    title: str
-    id: str
-    outlets: List[str]
-    countries: List[str]
-    keywords: List[str]
-    summary: str
-    disproof: str
-    languages: List[str]
-    publications: List[Tuple[str, str]]
+    iso_date: str = ''
+    title: str = ''
+    id: str = ''
+    outlets: List[str] = []
+    countries: List[str] = []
+    keywords: List[str] = []
+    summary: str = ''
+    disproof: str = ''
+    languages: List[str] = []
+    publications: List[Tuple[str, str]] = []
 
     def __init__(self, entry):
+        # Get basic data from database entry
         try:
-            # Get basic data from database entry
             date = get_data_column(entry, 'Date').contents[0].strip()
             self.iso_date = datetime.strptime(date, '%d.%m.%Y').date().isoformat()
             title_link = get_data_column(entry, 'Title').find('a')
@@ -52,24 +53,36 @@ class Entry:
             self.outlets = [o.strip() for o in get_data_column(entry, 'Outlets').contents[0].split(',')]
             self.countries = [c.strip() for c in get_data_column(entry, 'Country').contents[0].split(',')]
         except Exception as exception:
-            raise MalformedDataError('malformed entry error', self.id) from exception
+            raise MalformedDataError('Malformed entry error', self.id) from exception
 
+        # Get keywords, summary, disproof from report page
         try:
-            # Get keywords, summary, disproof from report page
             report = BeautifulSoup(req.get(self.id).text, 'html.parser')
 
-            self.keywords = report.find(text='Keywords:').parent.parent.contents[-1].strip()
-            self.summary = report.find(attrs={'class': 'b-report__summary-text'}).text.strip()
-            self.disproof = report.find(attrs={'class': 'b-report__disproof-text'}).text.strip()
+            try:
+                self.keywords = [k.strip() for k in report.find(text='Keywords:').parent.next.next.split(',')]
+            except AttributeError:
+                self.warn_missing('keywords')
+            try:
+                self.summary = report.find(attrs={'class': 'b-report__summary-text'}).text.strip()
+            except AttributeError:
+                self.warn_missing('summary')
+            try:
+                self.disproof = report.find(attrs={'class': 'b-report__disproof-text'}).text.strip()
+            except AttributeError:
+                self.warn_missing('disproof')
         except Exception as exception:
-            raise MalformedDataError('malformed report error', self.id) from exception
+            raise MalformedDataError('Malformed report error', self.id) from exception
 
+        # Get data on publications from report page
         try:
-            # Get data on publications from report page
-            publications = report.find_all(attrs={'class': 'b-catalog__link'})
-            self.languages = [lang.strip() for lang in report.find(text='Language/target audience:').next.split(',')]
-            assert (len(publications) == len(self.outlets))
+            try:
+                self.languages = [lang.strip() for lang in
+                                  report.find(text='Language/target audience:').next.split(',')]
+            except AttributeError:
+                self.warn_missing('languages')
 
+            publications = report.find_all(attrs={'class': 'b-catalog__link'})
             self.publications = []
             for publication in publications:
                 links = [a['href'] for a in publication.find_all('a')]
@@ -77,7 +90,10 @@ class Entry:
                 l1, l2 = links[0], links[1] if len(links) == 2 else None
                 self.publications.append((l1, l2))
         except Exception as exception:
-            raise MalformedDataError('malformed publications error', self.id) from exception
+            raise MalformedDataError('Malformed publications error', self.id) from exception
+
+    def warn_missing(self, name: str):
+        warn(f"Missing data '{name}' for {self.id}")
 
 
 class MalformedDataError(Exception):
@@ -86,10 +102,16 @@ class MalformedDataError(Exception):
         self.id = id
 
 
+class MissingDataError(Exception):
+    def __init__(self, name: str, id: str):
+        self.name = name
+        self.id = id
+
+
 def extract(entries):
-    post_keys = ['date', 'id', 'title', 'countries', 'keywords', 'languages']
+    post_keys = ['date', 'id', 'title', 'countries', 'keywords', 'languages', 'outlets']
     annotation_keys = ['id', 'summary', 'disproof']
-    publication_keys = ['id', 'outlet', 'publication', 'archive']
+    publication_keys = ['id', 'publication', 'archive']
 
     with open('posts.csv', 'w') as posts_file, \
             open('annotations.csv', 'w') as annotations_file, \
@@ -105,7 +127,7 @@ def extract(entries):
             try:
                 entry = Entry(entry_html)
             except MalformedDataError as mde:
-                print(f'WARNING: {repr(mde)} from {repr(mde.__cause__)}')
+                warn(f"WARNING: {repr(mde)} from {repr(mde.__cause__)}")
 
             posts_writer.writerow({
                 'date': entry.iso_date,
@@ -113,7 +135,8 @@ def extract(entries):
                 'title': entry.title,
                 'countries': entry.countries,
                 'keywords': entry.keywords,
-                'languages': entry.languages
+                'languages': entry.languages,
+                'outlets': entry.outlets
             })
 
             annotations_writer.writerow({
@@ -122,11 +145,9 @@ def extract(entries):
                 'disproof': entry.disproof
             })
 
-            for publication, outlet in zip(entry.publications, entry.outlets):
-                link, archived_link = publication
+            for link, archived_link in entry.publications:
                 publications_writer.writerow({
                     'id': entry.id,
-                    'outlet': outlet,
                     'publication': link,
                     'archive': archived_link
                 })
