299808cb1fa6fd53887ad75f1a81896aaf07b560
Christian Knabenhans <christian@knabenhans.fr>
2020-09-22 11:53:38 +0200

    Add co-occurrence computation class

---
+++ analysis/cooccurrence.py
@@ -0,0 +1,19 @@
+from collections import defaultdict
+from typing import Dict, Sequence
+
+import pandas as pd
+
+
+class CoOccurrence:
+    M: Dict[str, Dict[str, int]]
+
+    def __init__(self):
+        self.M = defaultdict(lambda: defaultdict(int))
+
+    def update(self, row_entities: Sequence[str], column_entries: Sequence[str]):
+        for r in row_entities:
+            for c in column_entries:
+                self.M[r][c] += 1
+
+    def get_dataframe(self) -> pd.Dataframe:
+        return pd.DataFrame.from_dict(self.M)
