6bdb30b1863aa6c24124dc91116655b93c64b781
Christian Knabenhans <christian@knabenhans.fr>
2020-10-17 21:07:07 +0200

    Add graph/network visualization capabilities

--- viz/visualize.py
+++ viz/visualize.py
@@ -2,6 +2,9 @@ import pandas as pd
 import plotly.graph_objects as go
 from pandas import DataFrame
 from plotly.subplots import make_subplots
+import numpy as np
+import networkx as nx
+import igraph as ig
 
 from analysis.cooccurrence import CoOccurrence
 
@@ -13,15 +16,22 @@ def explode_replace(data: DataFrame, old_name: str, new_name: str):
     return data
 
 
-def sort_by_sum(data: DataFrame) -> DataFrame:
+def sort(data: DataFrame) -> DataFrame:
+    """
+    Sort the rows and columns of :param data: by the sum of the row/column, using the row/column name to break ties.
+    """
+    # Compute sums of row/column
     sum_of_rows = data.sum(axis='columns')
     sum_of_cols = data.sum(axis='index')
     data['sum_of_rows'] = sum_of_rows
     data.loc['sum_of_cols'] = sum_of_cols
+    # Use MergeSort, which is a stable sorting algorithm
     return data \
-        .sort_values(by='sum_of_rows', ascending=False, axis='index') \
+        .sort_index(axis='index') \
+        .sort_index(axis='columns', kind='mergesort') \
+        .sort_values(by='sum_of_rows', ascending=False, axis='index', kind='mergesort') \
         .drop('sum_of_rows', axis='columns') \
-        .sort_values(by='sum_of_cols', ascending=False, axis='columns') \
+        .sort_values(by='sum_of_cols', ascending=False, axis='columns', kind='mergesort') \
         .drop('sum_of_cols', axis='index')
 
 
@@ -97,6 +107,65 @@ def plot_sankey(data: DataFrame, min_val: int = 50):
     fig.show()
 
 
+def plot_graph(data: DataFrame, min_val: int = 0):
+    # https://plotly.com/python/v3/3d-network-graph/
+    # https://towardsdatascience.com/python-interactive-network-visualization-using-networkx-plotly-and-dash-e44749161ed7
+    # https://plotly.com/python/network-graphs/#create-network-graph
+    data = data.fillna(0)
+    assert list(data.index) == list(data.columns)
+    assert np.allclose(data.values, data.transpose().values, equal_nan=True)
+    G = ig.Graph.Adjacency((data.values > min_val).tolist())
+    G.es['weight'] = data[data > min_val].values
+    layout = G.layout('sphere', dim=3)
+    Xn, Yn, Zn = list(zip(*layout))
+    Xe = []
+    Ye = []
+    Ze = []
+    We = []
+    traces = []
+    for e in G.es:
+        s = e.source
+        t = e.target
+        xs =[[layout[s][0], layout[t][0], None]]
+        ys = [[layout[s][1], layout[t][1], None]]
+        zs = [[layout[s][2], layout[t][2], None]]
+        w = data.values[s][t]
+        Xe += [layout[s][0], layout[t][0], None]
+        Ye += [layout[s][1], layout[t][1], None]
+        Ze += [layout[s][2], layout[t][2], None]
+
+        # TODO: bin edges by weight (decile?) to have coarse width variation
+        # edge_trace = go.Scatter3d(
+        #     x=xs, y=ys, z=zs,
+        #     mode='lines',
+        #     line=dict(
+        #         width=w
+        #     )
+        # )
+        # traces.append(edge_trace)
+
+    traces.append(go.Scatter3d(
+        x=Xe, y=Ye, z=Ze,
+        mode='lines'
+    ))
+
+    min_size, max_size = 2, 20  # in px
+    widths = data.sum(axis=0)
+    widths /= widths.max()  # in [0, 1]
+    widths = min_size + (max_size - min_size) * widths  # in [min_size, max_size]
+
+    node_trace = go.Scatter3d(
+        x=Xn, y=Yn, z=Zn,
+        mode='markers',
+        marker=dict(
+            size=widths
+        )
+    )
+
+    fig = go.Figure(data=traces + [node_trace])
+    fig.show()
+
+
 if __name__ == '__main__':
     cols = ['date', 'id', 'countries', 'languages', 'keywords']
     dtypes = {c: 'string' for c in cols}
@@ -110,10 +179,16 @@ if __name__ == '__main__':
     for ls, k in zip(df['languages'], df['keywords']):
         LK_cooc.update(ls.split('+'), k.split('+'))
 
-    LC = sort_by_sum(LC_cooc.get_dataframe())
-    LK = sort_by_sum(LK_cooc.get_dataframe())
+    KK_cooc = CoOccurrence()
+    for k in df['keywords']:
+        KK_cooc.update(k.split('+'), k.split('+'))
 
-    plot_sankey(LC.transpose())
+    LC = sort(LC_cooc.get_dataframe())
+    LK = sort(LK_cooc.get_dataframe())
+    KK = sort(KK_cooc.get_dataframe())
+
+    plot_graph(KK)
+    # plot_sankey(LC.transpose())
     # plot_marginal(LC)
     # plot_marginal(LK)
 
