aba19885b274f0dbc38acd6712efc9490b18d8f5
Christian Knabenhans <christian@knabenhans.fr>
2020-12-27 19:35:41 +0100

    Add total to database timeline, Improve legend

--- apps/database/callbacks.py
+++ apps/database/callbacks.py
@@ -10,14 +10,31 @@ from apps.database.data import date_language
 def update_timeline():
     # TODO: add index to timeline
     # TODO: average over month -> get top keywords in each language for each months
-
+    # TODO: add total as a grouped/stacked bar behind all others: see https://github.com/plotly/plotly.js/issues/4914
     sorted_dl = date_language.sum(axis=0).sort_values(ascending=False)
-    total_dl = date_language.sum(axis=1)
-    top = sorted_dl.drop('Other')[:5].index
+    total_dl = date_language.groupby(pd.Grouper(freq='M')).sum().sum(axis=1)
+    top = sorted_dl[:10].index
 
     month_year = date_language.index.strftime('%b %Y')
 
     traces = [
+        go.Scatter(
+            x=total_dl.index.strftime('%b %Y'),
+            y=total_dl,
+            mode='markers',
+            marker=dict(
+                symbol='cross-thin',
+                color='black',
+                line=dict(
+                    width=1,
+                ),
+            ),
+
+            name='Total'
+        )
+    ]
+
+    traces += [
         go.Bar(
             x=month_year,
             y=date_language[col],
@@ -31,12 +48,18 @@ def update_timeline():
 
     layout = go.Layout(
         title='Timeline',
-        paper_bgcolor='rgba(0,0,0,0)',
-        plot_bgcolor='rgba(0,0,0,0)',
+        # paper_bgcolor='rgba(0,0,0,0)',
+        # plot_bgcolor='rgba(0,0,0,0)',
         barmode='stack',
-        xaxis=dict(
-            tickangle=-45
+        yaxis=dict(
+            title='Entries in the Disinfo database',
+            # spikedash='dot',
+            showgrid=True
         ),
+        legend=dict(
+            traceorder='normal'
+        ),
+        height=800
     )
     fig = go.Figure(data=traces, layout=layout)
     fig.update_xaxes(rangeslider_visible=True)
